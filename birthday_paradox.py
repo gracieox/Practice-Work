ad8f93c2569192f8df704a4350646e4ff538e6f8
gracieox <gracie.oxnam@gmail.com>
2024-12-17 11:38:02 -0700

    Add files via upload

---
+++ birthday_paradox.py
@@ -0,0 +1,13 @@
+#gracie oxnam
+#birthday paradox prblem
+
+import random as r
+import datetime
+#day 0 = Augest 1, 1970
+    #first unic server turned on
+    #date object -> number of seconds since
+
+date.object
+#year_day = [i for i in range (1,366)]
+
+#today = datetime
